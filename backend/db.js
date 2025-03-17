0880106fa3a377f6c500c1c6aa074fc46fe9a1df
sassius <145041644+sassius@users.noreply.github.com>
2025-03-18 00:42:10 +0530

    paytm schema created

---
+++ backend/db.js
@@ -0,0 +1,15 @@
+require("dotenv").config();
+const mongoose = require("mongoose");
+
+mongoose.connect(process.env.MONGODB_URL);
+const userSchema = new mongoose.Schema({
+  username: String,
+  password: String,
+  firstName: String,
+  lastname: String,
+});
+const user = mongoose.model("User", userSchema);
+
+module.exports = {
+    User
+}
