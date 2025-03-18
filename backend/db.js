600c8368922abcbb4b579a87dbb522ba571cc6ff
sassius <145041644+sassius@users.noreply.github.com>
2025-03-19 01:38:44 +0530

    payment schema created

--- backend/db.js
+++ backend/db.js
@@ -41,6 +41,21 @@ const userSchema = new mongoose.Schema({
 });
 const User = mongoose.model("User", userSchema);
 
+const paymentSchema = new mongoose.Schema({
+  userId : {
+    required :true,
+    ref:'User',
+    type:mongoose.Schema.Types.ObjectId
+  },
+  balance : {
+    required : true,
+    type : Number
+  }
+})
+
+const Account = mongoose.model("Account",paymentSchema)
+
 module.exports = {
-    User
+    User,
+    Account,
 }
--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,23 @@
 const express = require("express");
 const cors = require("cors");
+require("dotenv").config();
 const app = express();
 //order matter cors and express.json should be above 
 app.use(cors());
 app.use(express.json());
 const mainRouter = require("./routes/index");
 app.use("/api/v1",mainRouter)
+app.get("/",(req,res)=>{
+    res.send("hello friend")
+})
+app.use((error, req, res, next) => {
+  console.log(error);
+  res.status(500).json({
+    message: "internal server error",
+  });
+});
 
-app.listen(3000);
+app.listen(3000, () => {
+  console.log("Server is running on port 3000 \n http://localhost:3000");
+});
 
